4ddf10b89b9594966b9b60130d36d7db40585ca8
Akilan <akilan7077@gmail.com>
2020-08-14 20:44:38 +0530

    commit

--- Todays.java
+++ Todays.java
@@ -7,8 +7,8 @@ public class LeapYear {
       year = sc.nextInt();
 
       if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-         System.out.println("Specified year is a leap year");
+         System.out.println("This is a leap year");
       else
-         System.out.println("Specified year is not a leap year");
+         System.out.println("This is not a leap year");
    }
 }
